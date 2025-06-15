e6cb65c10cf1e1a2a5d02eafef07334271c9bb40
mniehus <manfred.niehus@isel.pt>
2025-06-15 02:50:47 +0100

    removed lines

--- source/conf.py
+++ source/conf.py
@@ -26,8 +26,6 @@ myst_enable_extensions = ["dollarmath"]
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
